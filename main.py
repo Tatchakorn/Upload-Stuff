ee26307c7fc3dc49f4ef161ecffeb1d23d7fd99e
Saitako <saitako@outlook.com>
2023-01-05 11:03:31 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Simulltaneously Upload Video(s) To 5 Platforms 
+(Tiktok, IG, FB, YT, VOOM)
+With PreDetermined Time.
+'''
+
+import argparse
+from pathlib import Path
+
+
+
+if __name__ == '__main__':
+    ...
